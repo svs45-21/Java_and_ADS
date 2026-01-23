70e80e3a2e0d9206a5e48e86e0511518837f53b6
svs45-21 <shree.nandha.45@gmail.com>
2026-01-23 10:18:06 +0530

    updated code

--- DictHashmap.java
+++ DictHashmap.java
@@ -1,8 +1,55 @@
-import java.util.HashMap
-
+import java.util.HashMap;
+import java.util.Map;
 class Hd{
     HashMap<Integer, String> obj = new HashMap<>();
 
     //Insert or Upadate 
+    void Insert(int rollNumber, String Name){
+        obj.put(rollNumber, Name);
+    }
+
+    //Search
+    String Search(int rollNumber){
+        return obj.get(rollNumber);
+    }
+
+    //Delete
+
+    void Delete(int rollNumber){
+         obj.remove(rollNumber);
+    }
+
+    //Display
     
+    void Display(){
+          for (Map.Entry<Integer, String> element : obj.entrySet()) {
+        System.out.println("Roll number = " + element.getKey() +
+                           ", Student name = " + element.getValue());
+        }
+    }
+}
+
+public class DictHashmap{
+    public static void main(String[] args){
+
+        Hd h = new Hd();
+
+        h.Insert(101,"ganga");
+        h.Insert(102,"kiran");
+        h.Insert(103,"shree");
+        
+        h.Display();
+        System.out.println("Afeter Inserting the key and value");
+        String Result = h.Search(102);
+        System.out.println("The search Result :" +Result);
+        
+        h.Delete(103);
+        h.Display();
+       
+
+        //modifing or update
+        h.Insert(102,"Shree");
+
+        h.Display();
+    }
 }
