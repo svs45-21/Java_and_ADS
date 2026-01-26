6b1b2fe9ed3de7559128155ef237910787cdf5ce
svs45-21 <shree.nandha.45@gmail.com>
2026-01-26 10:48:22 +0530

    updated

--- java_programs/Operators.java
+++ java_programs/Operators.java
@@ -5,6 +5,24 @@ public calss Operators{
         //example (=)
         int num1 = 20; // syntax is datatype variable = litreal; where = is the assignment operator
 
+        //Arithmetic operators
+        //examples (+, -, *, /, %)
+        int sum = num1 + 10; // addition
+        int difference = num1 - 5; // subtraction
+        int product = num1 * 2; // multiplication
+        int quotient = num1 / 4; // division
+        int remainder = num1 % 3; // modulus  
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);  
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
 
+        //floating Number arithmetic
+        double num2 = 15.5;
+        double floatSum = num2 + 4.5;
+        System.out.println("Floating Point Sum: " + floatSum);  
+        System.out.println("num2 : " + num2);
+         
     }
 }
